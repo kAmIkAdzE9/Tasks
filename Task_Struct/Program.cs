660a99796623d6aca0cd3c72ed767f5be033a2ab
InterLink Intern <intern@intern-pc.interlink.office>
2021-10-19 14:15:10 +0300

    Task_Struct/Program.cs was updated

--- Task_Struct/Program.cs
+++ Task_Struct/Program.cs
@@ -9,13 +9,31 @@ namespace Task_Struct
             public string desc;
             public string done;
             public DateTime dueDate;
+            public int indentifier;
         }
         static void PrintTask(Task task) {
-            Console.WriteLine($"");
+            string date = "";
+            if(task.dueDate != new DateTime())
+                date = $"({task.dueDate.ToString("MMM dd")})";
+            Console.WriteLine($"{task.indentifier}. {task.done} {task.title} {date}\n{task.desc}");
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task task;
+            task.title = "Implement task output";
+            task.done = "[X]";
+            task.dueDate = new DateTime(2021, 08, 25);
+            task.desc = "Use fields: title, desc, done, dueData";
+            task.indentifier = 1;
+            PrintTask(task);
+
+            Task task2;
+            task2.indentifier = 2;
+            task2.title = "some title";
+            task2.done = "[]";
+            task2.desc = "";
+            task2.dueDate = new DateTime();
+            PrintTask(task2);
         }
     }
 }
