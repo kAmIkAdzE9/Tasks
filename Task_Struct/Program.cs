315e17c1b364386176b68928368963eb79b9f20d
InterLink Intern <intern@intern-pc.interlink.office>
2021-10-19 12:53:12 +0300

    Task_Struct/Program.cs was updated

--- Task_Struct/Program.cs
+++ Task_Struct/Program.cs
@@ -4,6 +4,15 @@ namespace Task_Struct
 {
     class Program
     {
+        struct Task {
+            public string title;
+            public string desc;
+            public string done;
+            public DateTime dueDate;
+        }
+        static void PrintTask(Task task) {
+            Console.WriteLine($"");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
