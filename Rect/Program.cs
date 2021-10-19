21f1e8b77ead0dc50d0ba7363998cc817f04f551
InterLink Intern <intern@intern-pc.interlink.office>
2021-10-19 15:14:27 +0300

    React/Program.cs was updated

--- Rect/Program.cs
+++ Rect/Program.cs
@@ -6,7 +6,92 @@ namespace Rect
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point pointA = new Point();
+            pointA.x = 1;
+            pointA.Print();
+
+            Point pointB = pointA.Move(3);
+            pointB.Print();
+
+            Rect rect = new Rect();
+            rect.width = 2;
+            rect.height = 1;
+
+            rect.Print();
+            rect.Scale(4);
+            rect.Print();
+
+            Console.WriteLine(rect.GetArea());
+            Console.WriteLine(rect.GetPerimeter());
+            Rect rect2 = rect.Move(4);
+            rect2.Print();
+
+            string str = "";
+            string[] arr = str.Split(',');
+            Console.WriteLine(arr.Length + " " + arr[0]);
+        }
+
+        struct Point
+        {
+            public int x;
+            public int y;
+
+            public void Print()
+            {
+                Console.WriteLine(this);
+            }
+
+            public override string ToString()
+            {
+                return $"(x: {x}, y: {y})";
+            }
+
+            public Point Move(int offset)
+            {
+                return new Point()
+                {
+                    x = this.x + offset,
+                    y = this.y + offset,
+                };
+            }
+        }
+
+        class Rect
+        {
+            public Point topLeft;
+            public int width;
+            public int height;
+
+            public void Print()
+            {
+                Console.WriteLine($"[{topLeft}; {width} x {height}]");
+            }
+
+            public void Scale(int factor)
+            {
+                width *= factor;
+                height *= factor;
+            }
+
+            public int GetArea()
+            {
+                return Math.Abs(width - topLeft.x) * Math.Abs(height - topLeft.y);
+            }
+
+            public int GetPerimeter()
+            {
+                return 2 * Math.Abs(width - topLeft.x) + 2 * Math.Abs(height - topLeft.y);
+            }
+
+            public Rect Move(int offset)
+            {
+                return new Rect()
+                {
+                    topLeft = topLeft.Move(offset),
+                    width = this.width + offset,
+                    height = this.height + offset
+                };
+            }
         }
     }
 }
