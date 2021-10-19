0499106ad6c727976b33d8935a0ed0d544571569
InterLink Intern <intern@intern-pc.interlink.office>
2021-10-19 15:31:53 +0300

    Task_Struct/Program.cs was updated

--- Task_Struct/Program.cs
+++ Task_Struct/Program.cs
@@ -7,21 +7,32 @@ namespace Task_Struct
         struct Task {
             public string title;
             public string desc;
-            public string done;
+            public bool done;
             public DateTime dueDate;
             public int indentifier;
         }
         static void PrintTask(Task task) {
+            string status = "";
+
+            if(task.done) {
+                status = "[X]";
+            }
+            else {
+                status = "[ ]";
+            }
+
             string date = "";
+
             if(task.dueDate != new DateTime())
                 date = $"({task.dueDate.ToString("MMM dd")})";
-            Console.WriteLine($"{task.indentifier}. {task.done} {task.title} {date}\n{task.desc}");
+
+            Console.WriteLine($"{task.indentifier}. {status} {task.title} {date}\n{task.desc}");
         }
         static void Main(string[] args)
         {
             Task task;
             task.title = "Implement task output";
-            task.done = "[X]";
+            task.done = true;
             task.dueDate = new DateTime(2021, 08, 25);
             task.desc = "Use fields: title, desc, done, dueData";
             task.indentifier = 1;
@@ -30,7 +41,7 @@ namespace Task_Struct
             Task task2;
             task2.indentifier = 2;
             task2.title = "some title";
-            task2.done = "[]";
+            task2.done = false;
             task2.desc = "";
             task2.dueDate = new DateTime();
             PrintTask(task2);
