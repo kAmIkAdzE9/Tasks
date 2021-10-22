c62a671429c8257c68c9c13ec0f1b059e9083d66
InterLink Intern <intern@intern-pc.interlink.office>
2021-10-22 09:40:41 +0300

    test for check auto open

--- OOP-Workshop/Tests/checkoutTest/CheckoutServiceTest.cs
+++ OOP-Workshop/Tests/checkoutTest/CheckoutServiceTest.cs
@@ -1,28 +1,47 @@
 using Xunit;
 
-namespace OOP_Workshop {
-    public class CheckoutServiceTest {
+namespace OOP_Workshop
+{
+    public class CheckoutServiceTest
+    {
         [Fact]
-        void closeCheck_withOneProduct() {
+        void closeCheck_withOneProduct()
+        {
             CheckoutService checkoutService = new CheckoutService();
             checkoutService.openCheck();
 
-            checkoutService.addProduct(new Product (7, "Milk"));
+            checkoutService.addProduct(new Product(7, "Milk"));
             Check check = checkoutService.closeCheck();
 
             Assert.Equal(check.getTotalCost(), 7);
         }
 
         [Fact]
-        void closeCheck_withTwoProduct() {
+        void closeCheck_withTwoProduct()
+        {
             CheckoutService checkoutService = new CheckoutService();
             checkoutService.openCheck();
 
-            checkoutService.addProduct(new Product (7, "Milk"));
-            checkoutService.addProduct(new Product (3, "Bred"));
+            checkoutService.addProduct(new Product(7, "Milk"));
+            checkoutService.addProduct(new Product(3, "Bred"));
             Check check = checkoutService.closeCheck();
 
             Assert.Equal(check.getTotalCost(), 10);
+        }
+
+        [Fact]
+        void addProduct__whenCheckIsClosed__opensNewCheck()
+        {
+            CheckoutService checkoutService = new CheckoutService();
+
+            checkoutService.addProduct(new Product(7, "Milk"));
+            Check milkCheck = checkoutService.closeCheck();
+            Assert.Equal(milkCheck.getTotalCost(), 7);
+
+            checkoutService.addProduct(new Product(3, "Bred"));
+            Check bredCheck = checkoutService.closeCheck();
+            Assert.Equal(bredCheck.getTotalCost(), 3);
+
         }
     }
 }
