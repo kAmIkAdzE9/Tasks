9eeca24e599f275a9ba0fb828d16e45e523f3919
InterLink Intern <intern@intern-pc.interlink.office>
2021-11-01 17:16:28 +0200

    ef-blog was created

---
+++ ef-blog/Migrations/20211101145807_InitialMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ef_blog.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "blogs",
+                columns: table => new
+                {
+                    blog_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    url = table.Column<string>(type: "text", nullable: true),
+                    rating = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_blogs", x => x.blog_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "posts",
+                columns: table => new
+                {
+                    post_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: true),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    blog_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_posts", x => x.post_id);
+                    table.ForeignKey(
+                        name: "fk_posts_blogs_blog_id",
+                        column: x => x.blog_id,
+                        principalTable: "blogs",
+                        principalColumn: "blog_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_posts_blog_id",
+                table: "posts",
+                column: "blog_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "posts");
+
+            migrationBuilder.DropTable(
+                name: "blogs");
+        }
+    }
+}
---
+++ ef-blog/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ef_blog
+{
+    class Program
+    {
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder
+                    .LogTo(Console.WriteLine)
+                    .UseSnakeCaseNamingConvention()
+                    .UseNpgsql("Host=127.0.0.1;Username=blog_app;Password=blog;Database=blog");
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Url { get; set; }
+            public int Rating { get; set; }
+            public List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public Blog Blog { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            using(var db = new BloggingContext()) {
+                var alexBlog = db.Blogs
+                    .Where(b => b.BlogId == 1)
+                    .Include(b => b.Posts)
+                    .Single();
+                
+                foreach(var post in alexBlog.Posts) {
+                    Console.WriteLine(post);
+                }
+
+                var secondPost = new Post() {
+                    Blog = alexBlog,
+                    Title = "My first post"
+                };
+
+                alexBlog.Posts.Add(secondPost);
+                
+                db.SaveChanges();
+            }
+        }
+    }
+}
