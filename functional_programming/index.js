d9f2fe7c96d6d0532b831391c125b28f5946ad36
InterLink Intern <intern@intern-pc.interlink.office>
2021-11-24 10:53:48 +0200

    Updated or, any, and, all predicate functions

--- functional_programming/index.js
+++ functional_programming/index.js
@@ -39,23 +39,38 @@ const rectangles = [
         "width": 7,
         "height": 7,
         "color": 'red'
+    },
+
+    {
+        "PointTopLeft": { "x": 0, "y": 0 },
+        "width": 108,
+        "height": 8,
+        "color": 'black'
+    },
+
+    {
+        "PointTopLeft": { "x": 0, "y": 0 },
+        "width": 20,
+        "height": 20,
+        "color": 'green'
     }
 ]
 
 let hasColor = c => r => r.color == c;
-let isRed = r => hasColor('red')(r);
-let isBlack = r => hasColor('black')(r);
+let isRed = hasColor('red');
+let isBlack = hasColor('black');
 let isSquare = r => r.width == r.height;
 let calcArea = r => r.width * r.height;
 let calcPerimeter = r => 2 * (r.width + r.height);
 let add = (a, b) => a + b;
 let max = (a, b) => Math.max(a, b);
-let filter = condition => list => list.filter(condition);
+let filter = predicate => list => list.filter(predicate);
 let map = fn => list => list.map(fn);
-let reduce = (fn, value) => list => list.reduce(fn, value);
+let reduce = (reducer, initVal) => list => list.reduce(reducer, initVal);
 let sum = reduce(add, 0);
+let maxFromList = reduce(max, 0);
 let flow = functions => data => functions.reduce((accum, func) => func(accum), data);
-let combine = functions => data => functions.reverse().reduce((accum, func) => func(accum), data);
+let combine = (...functions) => flow(functions.reverse())
 // function flow(functions, data) {
 //     if(functions.length == 0) {
 //         return data;
@@ -66,44 +81,52 @@ let combine = functions => data => functions.reverse().reduce((accum, func) => f
 //     return flow(new_functions, new_list);
 // }
 
-let or = (a, b) => a || b;
-let and = (a, b) => a && b;
+let or = (a, b) => value => a(value) || b(value); 
+let and = (a, b) => value => a(value) && b(value);
+let any = (...functions) => functions.reduce(or);
+let all = (...functions) => functions.reduce(and);
 
-let combinedCond = (conditions, predicate) => {
-    if(conditions.length == 0) {
-        return;
-    }
-    let result = conditions[0];
-    for(let cond of conditions) {
-        result = predicate(cond, result);
-    }
-    return result;
-}
+// let or = a => b => a || b;
+// let and = a => b => a && b;
 
-let any = conditions => combinedCond(conditions, or);
-let all = conditions => combinedCond(conditions, and);
+// let combinedPredicate = functions => predicate => data => {
+//     if(functions.length == 0) {
+//         return () => {return true};
+//     }
+//     let result = functions[0](data);
+//     for(let func of functions) {
+//         result = predicate(result)(func(data));
+//     }
+//     return result;
+// }
 
+// let any = functions  => combinedPredicate(functions)(or);
+// let all = functions  => combinedPredicate(functions)(and);
 
+//must be 36
 let maxAreaOfBlackSquares = flow([
-    filter(all([isBlack, isSquare])), 
-    map(calcArea), 
-    reduce(max, 0)]
+    filter(all(isBlack, isSquare)),
+    map(calcArea),
+    maxFromList])
+(rectangles);
+
+let maxAreaOfRedSquares = combine(
+    maxFromList,
+    map(calcArea),
+    filter(all(isSquare, isRed))
 )
 (rectangles);
 
-let totalPerimeterOfRedRectangles = combine([
+//must be 66
+let totalPerimeterOfRedRectangles = combine(
     sum,
     map(calcPerimeter), 
-    filter(isRed)]
+    filter(isRed)
 )
 (rectangles);
 
 //logging
-console.log(rectangles);
-
-rectangles.forEach(r => {
-    console.log(`Is red: ${isRed(r)}; Is black: ${isBlack(r)}`);
-});
-
+//console.log(rectangles);
 console.log('Max area of black squares is ' + maxAreaOfBlackSquares);
 console.log('Total perimeter of red rectangles is ' + totalPerimeterOfRedRectangles);
+console.log('Max area of red squares is ' + maxAreaOfRedSquares);
